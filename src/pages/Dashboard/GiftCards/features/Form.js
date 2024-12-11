f61c2e0ad660ebcbfad71a5c4d68c49a483774b2
johnoris <johnorisanwo@gmail.com>
2024-12-11 16:01:34 +0100

    updates

--- src/pages/Dashboard/GiftCards/features/Form.js
+++ src/pages/Dashboard/GiftCards/features/Form.js
@@ -8,7 +8,7 @@ import { ReactComponent as Close } from "assets/icons/close-x.svg";
 import Button from "components/General/Button/Button";
 import Input from "components/General/Input/Input";
 import Select from "components/General/Input/Select";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import GiftCardsStore from "../store";
 import { successToast } from "components/General/Toast/Toast";
 import ImagePicker from "components/General/Input/ImagePicker";
@@ -73,12 +73,16 @@ export default function Form({ details, toggler }) {
       });
     }
   };
+  const { warehouse_id } = useParams();
 
   return (
     <div className="gap-y-4 py-4 w-full h-full pb-4 overflow-y-auto">
       {details?.link ? (
         <div className="mb-5">
-          <Link to={details?.link} className="scale-90">
+          <Link
+            to={`/dashboard/gift-cards/${warehouse_id}`}
+            className="scale-90"
+          >
             <ArrowBack />
           </Link>
         </div>
@@ -89,7 +93,7 @@ export default function Form({ details, toggler }) {
       )}
 
       {details?.isAdd ? (
-        <h2 className="section-heading my-8 text-xl">Add Gift Card</h2>
+        <h2 className="section-heading my-8 text-xl">Add Gift Card Design</h2>
       ) : (
         <h2 className="section-heading mb-3 text-xl">Edit Gift Card Design</h2>
       )}
@@ -125,7 +129,7 @@ export default function Form({ details, toggler }) {
         <Button
           onClick={() => setFormTwo({ ...formTwo, showFormError: true })}
           type="submit"
-          text={details?.isAdd ? "Add Gift Card" : "Save Changes"}
+          text={details?.isAdd ? "Add Gift Card Design" : "Save Changes"}
           className="mt-8 mb-5"
           fullWidth
           isLoading={uploadingImage || createGiftCardLoading}
