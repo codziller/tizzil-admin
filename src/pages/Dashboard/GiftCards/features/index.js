97240f6d6905683a0140c24866f64e7bf8d535d0
johnoris <johnorisanwo@gmail.com>
2024-12-11 20:31:36 +0100

    adding stats

--- src/pages/Dashboard/GiftCards/features/index.js
+++ src/pages/Dashboard/GiftCards/features/index.js
@@ -2,26 +2,55 @@ import React, { useEffect, useState } from "react";
 
 import moment from "moment";
 import _ from "lodash";
-import qs from "query-string";
 import { Link, useParams } from "react-router-dom";
-
-import useTableFilter from "hooks/useTableFilter";
-import ActiveFilter from "components/General/ActiveFilter";
 import CircleLoader from "components/General/CircleLoader/CircleLoader";
 import Table from "components/General/Table";
 import { pageCount, promos } from "utils/appConstant";
-import { hasValue } from "utils/validations";
 import { ReactComponent as SearchIcon } from "assets/icons/SearchIcon/searchIcon.svg";
 import { ReactComponent as Plus } from "assets/icons/add.svg";
 import useWindowDimensions from "hooks/useWindowDimensions";
-import { paramsObjectToQueryString } from "utils/request";
-import { transactionAmount } from "utils/transactions";
 import TransactionDetailsModal from "./DetailsModal";
-import SearchBar from "components/General/Searchbar/SearchBar";
 import { Button } from "components/General/Button";
-import TableDropdown from "components/General/Dropdown/TableDropdown";
 import GiftCardsStore from "../store";
 import { observer } from "mobx-react-lite";
+import { ReactComponent as IncomeIcon } from "assets/icons/income-icon.svg";
+import dateConstants from "utils/dateConstants";
+import DashboardFilterDropdown from "components/General/Dropdown/DashboardFilterDropdown";
+import EarningCard from "pages/Dashboard/Home/features/EarningCard";
+
+export const dateFilters = [
+  {
+    value: "today",
+    label: "Today",
+    start_date: dateConstants?.today,
+    end_date: dateConstants?.today,
+  },
+  {
+    value: "yesterday",
+    label: "Yesterday",
+    start_date: dateConstants?.yesterday,
+    end_date: dateConstants?.yesterday,
+  },
+  {
+    value: "this_week",
+    label: "This Week",
+    start_date: dateConstants?.startOfWeek,
+    end_date: dateConstants?.endOfWeek,
+  },
+  {
+    value: "all_time",
+    label: "All Time",
+    start_date: dateConstants?.firstDay,
+    end_date: dateConstants?.today,
+  },
+
+  {
+    value: "custom",
+    label: "Custom Date",
+    start_date: dateConstants?.startOfWeek,
+    end_date: dateConstants?.endOfWeek,
+  },
+];
 
 const GiftCardsPage = () => {
   const { width, isMobile } = useWindowDimensions();
@@ -32,7 +61,14 @@ const GiftCardsPage = () => {
 
   const searching = "";
 
-  const { getGiftCards, giftCards, loading } = GiftCardsStore;
+  const {
+    getGiftCards,
+    giftCards,
+    loading,
+    giftCardStats,
+    giftCardStatsLoading,
+    getGiftCardStats,
+  } = GiftCardsStore;
 
   useEffect(() => {
     getGiftCards({ data: { page: currentPage } });
@@ -41,6 +77,7 @@ const GiftCardsPage = () => {
   useEffect(() => {
     console.log({ giftCards });
   }, [giftCards]);
+
   const columns = [
     {
       name: "Card Design",
@@ -101,11 +138,92 @@ const GiftCardsPage = () => {
   };
 
   useEffect(() => scrollToTop(), [promos]);
+  const [dateFilter, setDateFilter] = useState(dateFilters[0]);
+
+  useEffect(() => {
+    const endDate = moment(dateFilter.end_date)
+      .add(1, "day")
+      .format("YYYY-MM-DD");
+
+    getGiftCardStats({
+      data: {
+        endDate,
+        startDate: moment(dateFilter.start_date).format("YYYY-MM-DD"),
+      },
+    });
+  }, [dateFilter]);
 
   return (
     <>
       <div className="h-full md:pr-4">
         <div className="flex flex-col justify-start items-start h-full w-full gap-y-5">
+          <div className="flex items-center w-fit mb-3 gap-1">
+            <div className="w-full sm:w-[200px]">
+              <DashboardFilterDropdown
+                placeholder="Filter by: "
+                options={dateFilters}
+                name="payout_filter"
+                onClick={(e) => {
+                  if (e.value === "custom") {
+                    setShowDateModal(true);
+                    return;
+                  }
+                  setDateFilter(e);
+                }}
+                value={dateFilter?.label}
+              />
+            </div>
+
+            <div className="flex justify-start items-center w-full truncate text-base">
+              {dateFilter.value === "today"
+                ? moment(dateFilter.start_date).format("MMM Do, YYYY")
+                : `${moment(dateFilter.start_date).format(
+                    "MMM Do, YYYY"
+                  )} - ${moment(dateFilter.end_date).format("MMM Do, YYYY")}`}
+            </div>
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 smlg:grid-cols-4 gap-4 justify-between items-start w-full mb-2">
+            <EarningCard
+              icon={<IncomeIcon className="scale-[0.8]" />}
+              title="Total Amount"
+              value={giftCardStats?.totalAmount}
+              isLoading={giftCardStatsLoading}
+              isAmount
+            />
+            <EarningCard
+              icon={<IncomeIcon className="scale-[0.8]" />}
+              title="Total Created"
+              value={giftCardStats?.totalCreated}
+              isLoading={giftCardStatsLoading}
+            />
+            <EarningCard
+              icon={<IncomeIcon className="scale-[0.8]" />}
+              title="Total Top-up Amount"
+              value={giftCardStats?.totalTopUpAmount}
+              isLoading={giftCardStatsLoading}
+              isAmount
+            />
+            <EarningCard
+              icon={<IncomeIcon className="scale-[0.8]" />}
+              title="Total Top-ups"
+              value={giftCardStats?.totalTopUps}
+              isLoading={giftCardStatsLoading}
+            />
+            {/* <EarningCard
+              icon={<IncomeIcon className="scale-[0.8]" />}
+              title="Average Initial Amount"
+              value={giftCardStats?.avgInitialAmount}
+              isLoading={giftCardStatsLoading}
+              isAmount
+            /> */}
+            <EarningCard
+              icon={<IncomeIcon className="scale-[0.8]" />}
+              title="Average Top-up Amount"
+              value={giftCardStats?.avgTopUpAmount}
+              isLoading={giftCardStatsLoading}
+              isAmount
+            />
+          </div>
           <div className="flex justify-between items-center w-full mb-3 gap-1">
             {/* <div className="w-full sm:w-[45%] sm:min-w-[300px]">
               <SearchBar
