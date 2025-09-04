1922ffccac83dbd4bd7d42c9c58d21300d68e4a6
Akinpeju Yusuf <akinpejuyusuf@bani.africa>
2025-09-04 16:40:00 +0200

    spacing to dashboard home bottom added

--- src/components/Dashboard/BrandDashboard/BrandDashboard.js
+++ src/components/Dashboard/BrandDashboard/BrandDashboard.js
@@ -153,7 +153,7 @@ const BrandDashboard = ({ userRole }) => {
       </div>
 
       {/* Main Content Split */}
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 pb-20">
         {/* Left Section - Graph */}
         <div className="lg:col-span-2 space-y-5">
           {/* Graph Card */}
